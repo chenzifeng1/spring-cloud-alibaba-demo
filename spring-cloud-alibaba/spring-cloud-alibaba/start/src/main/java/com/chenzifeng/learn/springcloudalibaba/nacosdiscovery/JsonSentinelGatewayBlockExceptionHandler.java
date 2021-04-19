959194796b9d2108357f14014d361799d3ef2841
chenzifeng1 <704734862@qq.com>
2021-04-19 20:00:08 +0800

    gateway整合sentinel
    没有注册到服务，没有测试路径转发

---
+++ spring-cloud-alibaba/spring-cloud-alibaba/start/src/main/java/com/chenzifeng/learn/springcloudalibaba/nacosdiscovery/JsonSentinelGatewayBlockExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.chenzifeng.learn.springcloudalibaba.nacosdiscovery;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebExceptionHandler;
+
+
+import java.util.List;
+
+/**
+ * @Author: czf
+ * @Description:
+ * @Date: 2021-04-17 10:41
+ * @Version: 1.0
+ **/
+@Slf4j
+
+public class JsonSentinelGatewayBlockExceptionHandler  {
+
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//    private List<ViewResolver> viewResolvers;
+//    private List<HttpMessageWriter<?>> messageWriters;
+//
+//    public JsonSentinelGatewayBlockExceptionHandler(List<ViewResolver> viewResolvers, ServerCodecConfigurer serverCodecConfigurer) {
+//        this.viewResolvers = viewResolvers;
+//        this.messageWriters = serverCodecConfigurer.getWriters();
+//    }
+//
+//    @Override
+//    public Mono<Void> handle(ServerWebExchange serverWebExchange, Throwable throwable) {
+//        return null;
+//    }
+
+
+}
